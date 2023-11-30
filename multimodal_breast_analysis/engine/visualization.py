21ad4ac93f6f78b4e69c0dbaeaf594f4f33bbdd9
Muhammad Al-Barbary <muhammad.albarbary1@gmail.com>
2023-11-30 20:50:23 +0100

    update sampling criteria

--- multimodal_breast_analysis/engine/visualization.py
+++ multimodal_breast_analysis/engine/visualization.py
@@ -5,18 +5,20 @@ from torchvision.ops.boxes import masks_to_boxes
 import matplotlib.pyplot as plt
 from torchvision.utils import draw_bounding_boxes
 import warnings
+from random import randint
 
-def visualize_batch(model, images, targets, class_name, figsize=(8,8)):
-    warnings.filterwarnings("ignore") #disable warning for empty boxes
+def visualize_batch(model, images, targets, class_name, figsize=(10,10)):
+    warnings.filterwarnings("ignore") #disbale warning for empty boxes
     model.eval()
     predictions = model(images)
-    pred = predictions[0]
-    image = images[0]
+    random_idx = randint(0, len(images) - 1)
+    pred = predictions[random_idx]
+    image = images[random_idx]
     image = (255.0 * (image - image.min()) / (image.max() - image.min())).to(torch.uint8)
     pred_labels = [f"{class_name}: {score:.3f}" for label, score in zip(pred["labels"], pred["scores"])]
     pred_boxes = pred["boxes"].long()  
-    target_labels = [f"{class_name}" for label in targets[0]["labels"]]
-    target_image = draw_bounding_boxes(image, targets[0]["boxes"], target_labels, colors="red");
+    target_labels = [f"{class_name}" for label in targets[random_idx]["labels"]]
+    target_image = draw_bounding_boxes(image, targets[random_idx]["boxes"], target_labels, colors="red");
     plt.figure(figsize=figsize)
     plt.subplot(1,2,1)
     plt.title("target")
